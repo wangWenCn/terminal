3e36fce646c4b6636cf5bf70aadb9e848232578f
wangwen <32148285@qq.com>
2024-04-15 21:55:09 +0800

    update

--- terminal.go
+++ terminal.go
@@ -1,14 +1,12 @@
 package terminal
 
-// 事件内容由后续再约定，此处先进行声明
 type Event struct {
-	EventCode int    `json:"eventCode" form:"eventCode"` //指令类别代码
-	Number    string `json:"number" form:"number"`       // 具体事件的为标识
-	Value     string `json:"value" form:"value"`         //指令中的可用值
+	EventCode int    `json:"eventCode" form:"eventCode"`
+	Number    string `json:"number" form:"number"`
+	Value     string `json:"value" form:"value"`
 }
 
 type MqttTerminal interface {
-	// XAuth(data map[string]interface{}) (metadata map[string]interface{}, err error)
 	Run()
 	Stop()
 	GetDevcieSn() string
@@ -17,15 +15,5 @@ type MqttTerminal interface {
 	GenerateOnlineMessage(clientID string) string
 	GenerateLastWillMessage(clientID string) string
 	GenerateLastWillTopic(clientID string) string
-	Separated() bool //是否接收发送分离
-}
-
-type MqttTerminalSender interface {
-	Sende(topic string, payload []byte, qos byte, retained bool, userProperties map[string]string)
-}
-
-type MqttTerminalRecHandler func()
-
-type MqttTerminalReceiver interface {
-	Receive(topic string, recHandler MqttTerminalRecHandler)
+	Separated() bool
 }
